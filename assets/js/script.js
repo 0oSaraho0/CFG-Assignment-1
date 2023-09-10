2958b6c6f73fb6d86829e2622a28cec99e31b42e
Sarah <0osarahtoddo0gmail.com>
2023-09-10 17:21:14 +0100

    add countdown timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,20 @@
 const services = [
-  {
-    name: "wash",
-    price: 2.2,
-  },
-  {
-    name: "dry",
-    price: 2.3,
-  },
-  {
-    name: "groom",
-    price: 5.2,
-  },
+"wash",
+"dry",
+"groom",
 ];
 const chosenServices = [];
 const container = document.getElementById("container");
-const groomingContainer = document.getElementById("groomingContainer")
+const groomingContainer = document.getElementById("grooming-container");
+const groomingContainer2 = document.getElementById("grooming-container2");
 const wash = document.getElementById("wash");
 const dry = document.getElementById("dry");
 const groom = document.getElementById("groom");
 const continueToGroom = document.getElementById("continue");
 const serviceContainer = document.getElementById("service-conf");
 let textService = document.getElementById("service-confirmation");
+const wetUni = document.createElement("img");
+const washBtn = document.createElement("button");
 console.log(textService);
 
 console.log(wash);
@@ -35,9 +29,9 @@ function hide(hiddenElement) {
   hiddenElement.classList.toggle("hide");
 }
 
-document.addEventListener('DOMContentLoaded',function(){
-    hide(container)
-})
+document.addEventListener("DOMContentLoaded", function () {
+  hide(container);
+});
 
 // A function add to pick a service
 function addService(e) {
@@ -47,7 +41,7 @@ function addService(e) {
     return service.name === serviceName;
   });
   // push the chosen service to the chosen service array
-  chosenServices.push(selectedService);
+  chosenServices.push(serviceName);
 
   let message = document.createElement("p");
   message.textContent = `Great we will ${serviceName} your unicorn!`;
@@ -61,11 +55,60 @@ function addService(e) {
 
   continueToGroom.addEventListener("click", function () {
     hide(container);
-    groomingTime()
+    groomingTime();
   });
-  
-};
-
-function groomingTime(){
-  hide(groomingContainer);
 }
+
+function groomingTime() {
+  hide(groomingContainer);
+  if (chosenServices.includes('wash')){
+    washBtn.innerHTML = "Start Wash";
+    washBtn.setAttribute("id", "startWash");
+    document.body.appendChild(washBtn);
+    let startWash = document.getElementById("startWash");
+    startWash.addEventListener("click", washTime);
+    console.log(services)
+}
+
+  }
+  
+
+function washTime() {
+  washBtn.remove();
+  console.log("washing now");
+  startCountdown(5);
+}
+
+function finishWashing(){
+  
+  wetUni.src = "assets/images/wet-unicorn.jpeg";
+  let scr = document.getElementbyId("groomingContainer2")
+  scr.appendChild(wetUni)
+  console.log(wetUni)
+
+  let finishWash = document.createElement('p');
+  finishWash.innerHTML = "Great she is all nice and clean";
+  finishWash.setAttribute('id', 'finish-wash');
+  document.body.appendChild(finishWash)
+  
+}
+
+function startCountdown(seconds) {
+  let counter = seconds;
+  const interval = setInterval(() => {
+    groomingContainer2.innerHTML = counter;
+    console.log(counter);
+    counter--;
+
+    if (counter < 0) {
+      clearInterval(interval);
+      if (chosenServices.includes('wash')){
+        finishWashing();
+      }
+      
+
+      
+    }
+  }, 1000);
+}
+
